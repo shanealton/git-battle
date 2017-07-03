39ab75e297648b1cf59abec729a9b94cd048d92e
Shane Alton <shanealton1@gmail.com>
2017-07-03 17:00:11 -0400

    Add container and presentational components
    Added directories for container, presentational, and the layout component.

--- app/index.js
+++ app/index.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './components/layout/App'
 require('./index.css')
-
-class App extends React.Component {
-  render() {
-    return (
-      <h2>Hello World!</h2>
-    )
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById('app'))
