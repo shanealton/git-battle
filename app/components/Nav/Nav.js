fddb25be87f89856950e752021a8d5d03fae6637
Shane Alton <shanealton1@gmail.com>
2017-07-04 15:58:05 -0400

    Add Nav bar Component

---
+++ app/components/Nav/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './styles.css'
+
+const Nav = (props) => {
+  return (
+    <section className='nav-container'>
+    <ul className='nav'>
+      <li>
+        <NavLink activeClassName='active' exact to='/'>Home</NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/battle'>Battle</NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName='active' to='/popular'>Popular</NavLink>
+      </li>
+    </ul>
+    </section>
+  )
+}
+
+export default Nav
