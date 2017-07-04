44c16bd9a71660f233f2529718a4203adf87d387
Shane Alton <shanealton1@gmail.com>
2017-07-03 22:25:33 -0400

    Add Repos component
    Updated popular component to fetch the most popular repos for the selected language.
    
    Added base styles to Repos component.

--- app/components/containers/Popular/Popular.js
+++ app/components/containers/Popular/Popular.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import api from '../../../utils/api'
 require('./styles.css')
 
 const SelectLanguage = (props) => {
@@ -20,25 +21,61 @@ const SelectLanguage = (props) => {
   )
 }
 
+const Repos = (props) => {
+  return (
+    <ul className="popular-list">
+      {props.repos.map((repo, i) => {
+        return (
+          <li key={repo.name} className="popular-item">
+            <div className="popular-rank">#{i + 1}</div>
+            <ul className="space-list-items">
+              <li>
+                <img className="avatar" src={repo.owner.avatar_url} alt={repo.owner.login} />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired
+}
+
+Repos.propTypes = {
+  repos: PropTypes.array.isRequired
 }
 
 export default class Popular extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     }
     this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
   }
 
   updateLanguage(lang) {
     this.setState(() => {
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       }
+    })
+
+    api.fetchPopularRepos(lang).then((repos) => {
+      this.setState({repos})
     })
   }
 
@@ -49,6 +86,7 @@ export default class Popular extends React.Component {
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
+        {!this.state.repos ? <p className="loading">Loading...</p> : <Repos repos={this.state.repos} />}
       </div>
     )
   }
