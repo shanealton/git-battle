b37f318644dc669997a1be0839340e2460b55064
Shane Alton <shanealton1@gmail.com>
2017-07-04 15:59:18 -0400

    Add react-rotuer routes to app

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import Nav from './Nav/Nav'
+import Home from './Home/Home'
+import Battle from './Battle/Battle'
 import Popular from './Popular/Popular'
 
 
@@ -7,10 +10,16 @@ export default class App extends React.Component {
   render() {
     return (
       <Router>
+      <section>
+      <Nav />
         <div className='container'>
-          <Route exact path='/' component={Popular} />
-          <Route path='/popular' component={Popular} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+          </Switch>
         </div>
+        </section>
       </Router>
     )
   }
