aa2e8d3bc76673d6d7b20acc8ac290fdb8491ccc
Shane Alton <shanealton1@gmail.com>
2017-07-04 15:58:31 -0400

    Add Home component

---
+++ app/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles.css'
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <section className='home-container'>
+        <h1>Github Battle: Battle your friends.</h1>
+        <Link className='button' to='/battle'>Battle</Link>
+      </section>
+    )
+  }
+}
