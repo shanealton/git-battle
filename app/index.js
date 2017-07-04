4ccb4dad4980e0ed3a31509679fa49c947f157d8
Shane Alton <shanealton1@gmail.com>
2017-07-03 20:12:42 -0400

    Add normalize.css
    Added normalize.css to package.json.

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/layout/App'
+import 'normalize.css'
 require('./index.css')
 
 ReactDOM.render(<App />, document.getElementById('app'))
