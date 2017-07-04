441e8b2a893b2b7cc13bf018d8a28a2f50f62beb
Shane Alton <shanealton1@gmail.com>
2017-07-03 20:41:51 -0400

    Create stateless component for languages
    Moved language selection UI creation into a stateless component.
    
    Added PropTypes to SelectLanguage.

--- app/components/containers/Popular/Popular.js
+++ app/components/containers/Popular/Popular.js
@@ -1,5 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 require('./styles.css')
+
+const SelectLanguage = (props) => {
+  let languages = ['All', 'JavaScript', 'Swift', 'Java', 'CSS', 'Python']
+  return (
+    <ul className="languages">
+      {languages.map((lang) => {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: 'palevioletred'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+            {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
 
 export default class Popular extends React.Component {
   constructor(props) {
@@ -7,6 +31,7 @@ export default class Popular extends React.Component {
     this.state = {
       selectedLanguage: 'All'
     }
+    this.updateLanguage = this.updateLanguage.bind(this)
   }
 
   updateLanguage(lang) {
@@ -18,20 +43,13 @@ export default class Popular extends React.Component {
   }
 
   render() {
-    let languages = ['All', 'JavaScript', 'Swift', 'Java', 'CSS', 'Python']
     return (
-      <ul className="languages">
-        {languages.map((lang) => {
-          return (
-            <li
-              style={lang === this.state.selectedLanguage ? {color: 'palevioletred'} : null}
-              onClick={this.updateLanguage.bind(this, lang)}
-              key={lang}>
-              {lang}
-            </li>
-          )
-        })}
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
     )
   }
 }
