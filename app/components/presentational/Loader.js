6226a12f002d378968b978daf192514b8bb6bda7
Shane Alton <shanealton1@gmail.com>
2017-07-05 09:15:56 -0400

    Add Loader component
    Added a component for the animated loading element.

---
+++ app/components/presentational/Loader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../index.css'
+
+const Loader = () => {
+  return (
+    <div className="spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Loader
