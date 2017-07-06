aa5b150090430e95051011bcbfd3438a78174e8a
Shane Alton <shanealton1@gmail.com>
2017-07-06 14:10:39 -0400

    Setup route for Results component
    Created a Results component to parse the usernames from props.
    
    Setup a route for Results. Navigates to Results after two players are selected and the user clicks the 'Battle' button.

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 import Nav from './Nav'
 import Home from './Home'
 import Battle from './Battle'
+import Results from './Results'
 import Popular from './Popular'
 
 
@@ -16,6 +17,7 @@ export default class App extends React.Component {
           <Switch>
             <Route exact path='/' component={Home} />
             <Route exact path='/battle' component={Battle} />
+            <Route path='/battle/results' component={Results} />
             <Route path='/popular' component={Popular} />
           </Switch>
         </div>
---
+++ app/components/Results.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Results extends React.Component {
+  render() {
+    return (
+      <section>Results</section>
+    )
+  }
+}
+
+export default Results
