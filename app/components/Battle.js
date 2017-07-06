280d597dfe7f3ea8378b24131f2b800563f6fc91
Shane Alton <shanealton1@gmail.com>
2017-07-06 13:21:52 -0400

    Add PlayerPreview component to Battle
    Added PlayerPreview component to Battle.
    
    Populates preview with player GitHub username and avatar.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -3,6 +3,33 @@ import PropTypes from 'prop-types'
 import Loader from './presentational/Loader'
 import '../index.css'
 
+const PlayerPreview = (props) => {
+  return (
+    <section>
+      <div className='column'>
+        <img
+          className='avatar'
+          src={props.avatar}
+          alt={props.username}
+        />
+        <h2 className='username'>@{props.username}</h2>
+      </div>
+      <button
+        className='reset'
+        onClick={props.onReset.bind(null, props.id)}>
+          Reset
+      </button>
+    </section>
+  )
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired
+}
+
 class PlayerInput extends React.Component {
   constructor(props) {
     super(props)
@@ -72,25 +99,60 @@ export default class Battle extends React.Component {
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleReset = this.handleReset.bind(this)
   }
 
   handleSubmit(id, username) {
     this.setState(() => {
       let newState = {}
       newState[id + 'Name'] = username
-      newState[id + 'Image'] = 'https://github.com' + username + '.png?size=200'
+      newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200'
+      return newState
+    })
+  }
+
+  handleReset(id) {
+    this.setState(() => {
+      let newState = {}
+      newState[id + 'Name'] = ''
+      newState[id + 'Image'] = null
       return newState
     })
   }
   render() {
-    let playerOneName = this.state.playerOneName
-    let playerTwoName = this.state.playerTwoName
+    const playerOneName = this.state.playerOneName,
+          playerTwoName = this.state.playerTwoName,
+          playerOneImage = this.state.playerOneImage,
+          playerTwoImage = this.state.playerTwoImage
 
     return (
       <section>
         <div className='row'>
-          {!playerOneName && <PlayerInput id='playerOne' label='Player One' onSubmit={this.handleSubmit} />}
-          {!playerTwoName && <PlayerInput id='playerTwo' label='Player Two' onSubmit={this.handleSubmit} />}
+          {!playerOneName &&
+            <PlayerInput
+              id='playerOne'
+              label='Player One'
+              onSubmit={this.handleSubmit} />}
+
+          {playerOneImage !== null &&
+            <PlayerPreview
+              avatar={playerOneImage}
+              username={playerOneName}
+              onReset={this.handleReset}
+              id='playerOne' />}
+
+          {!playerTwoName &&
+            <PlayerInput
+              id='playerTwo'
+              label='Player Two'
+              onSubmit={this.handleSubmit} />}
+
+          {playerTwoImage !== null &&
+            <PlayerPreview
+              avatar={playerTwoImage}
+              username={playerTwoName}
+              onReset={this.handleReset}
+              id='playerTwo' />}
         </div>
       </section>
     )
